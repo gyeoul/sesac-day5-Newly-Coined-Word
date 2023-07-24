63620c6e6d3d9c1083803bf4cf223348537075fe
Gyeoul <clsmay.1st@gmail.com>
2023-07-24 16:50:40 +0900

    Initial Commit

---
+++ sesac-day5-newlyCoinedWord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sesac-day5-newlyCoinedWord
+//
+//  Created by 박창현 on 2023.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
